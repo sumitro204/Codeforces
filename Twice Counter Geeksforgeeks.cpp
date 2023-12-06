0a3dd3cb11c98319a990f3a4eafc94ce01b835b9
Karnel Chakma <120170336+sumitro204@users.noreply.github.com>
2023-12-06 23:00:17 +0600

    Create Twice Counter Geeksforgeeks.cpp

---
+++ Twice Counter Geeksforgeeks.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+        int countWords(string list[], int n)
+        {
+           map<string,int>m;
+           for(int i=0; i<n; i++){
+               m[list[i]]++;
+           }
+           int x = 0;
+           for(auto it : m){
+               if(it.second == 2){
+                   x++;
+               }
+           }
+           return x;
+           
+           
+        }
+
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string list[n];
+        for(int i=0;i<n;i++)
+            cin>>list[i];
+        Solution ob;    
+        cout <<ob.countWords(list, n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
