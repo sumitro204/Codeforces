0a9a9ac778b7d54db0d22f4bff5885cf682b13d5
Karnel Chakma <120170336+sumitro204@users.noreply.github.com>
2023-12-12 21:48:35 +0600

    Create A. Dreamoon and Stairs.cpp

---
+++ A. Dreamoon and Stairs.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int res = 0;
+
+    if (m > n)
+    {
+        res = -1;
+    }
+
+    else
+    {
+        if (n % 2)
+        {
+            res = (n / 2) + 1;
+        }
+        else
+        {
+            res = n / 2;
+        }
+        while (res % m > 0)
+        {
+            res++;
+        }
+    }
+
+    cout << res << endl;
+    return 0;
+}
