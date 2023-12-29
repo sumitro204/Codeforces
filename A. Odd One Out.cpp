ae8ebe89f8324fbff5ad7502053e3a9508667bed
Karnel Chakma <120170336+sumitro204@users.noreply.github.com>
2023-12-29 10:13:58 +0600

    Create A. Odd One Out.cpp

---
+++ A. Odd One Out.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long long t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a==b){
+            cout<< c <<endl;
+        }
+        if(a==c){
+            cout<< b <<endl;
+        }
+        if(b==c){
+            cout<< a <<endl;
+        }
+        
+    }
+ 
+  return 0;
+}  
